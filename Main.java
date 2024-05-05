c66e1fd7a6cd13b2f1adb1ebf0856670dc9caeec
Pawel Stanieta <pawelas000@gmail.com>
2024-05-05 10:00:51 +0200

    1

--- Main.java
+++ Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
         List<Employee> employees = new ArrayList<>();
 
-        // instancje
+        
         employees.add(new Worker("Jack", 3000, 1, "01-01-2020", "Developer"));
         employees.add(new Worker("Jane", 3200.5, 2, "02-02-2020", "Developer"));
         employees.add(new Worker("John", 3000, 1, "01-01-2020", "Developer")); // Duplicate ID as the first
@@ -21,23 +21,23 @@ public class Main {
         employees.add(new Worker("Jasper", 3150.5, 3, "07-07-2020", "Developer")); // Duplicate ID with Jake
         employees.add(new Manager("Julia", 5200.5, 1, "08-08-2020", "IT Manager")); // Duplicate ID with John
 
-        // łączna kwota do wypłaty
+       
         double totalSalary = employees.stream().mapToDouble(Employee::getSalary).sum();
         double managerTotalSalary = employees.stream().filter(e -> e instanceof Manager).mapToDouble(Employee::getSalary).sum();
         double workerTotalSalary = employees.stream().filter(e -> e instanceof Worker).mapToDouble(Employee::getSalary).sum();
 
-        // wypłata
-        System.out.println("Łączna kwota do wyplaty: " + totalSalary);
-        System.out.println("Łączna kwota do wyplaty dla menadzerow: " + managerTotalSalary);
-        System.out.println("Łączna kwota do wyplaty dla pracownikow: " + workerTotalSalary);
+        
+        System.out.println("Łączna kwota wynagrodzenia wszystkich pracowników: " + totalSalary);
+        System.out.println("Łączna kwota wynagrodzenia dla menadzerow: " + managerTotalSalary);
+        System.out.println("Łączna kwota wynagrodzenia dla pracownikow: " + workerTotalSalary);
 
-        // duplikat
+        
         Map<Integer, List<Employee>> employeeMap = new HashMap<>();
         for (Employee emp : employees) {
             employeeMap.computeIfAbsent(emp.getId(), k -> new ArrayList<>()).add(emp);
         }
 
-        // duplikat szczegóły
+       
         employeeMap.forEach((id, emps) -> {
             if (emps.size() > 1) {
                 System.out.println("Duplikat ID " + id + " W przypadku pracowników:");
